43e307572e0933748676e701e51427bfb60641d7
Valentin P <valentin.porcellini@gmail.com>
2020-03-29 23:18:53 -0700

    added connection to mongodb cloud

--- server/app.js
+++ server/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 
 const schema = require('./schema/schema')
 
 const app = express();
+
+mongoose.connect(
+  "connectionstring",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to the database');  
+});
+
 
 app.use('/graphql', graphqlHTTP({
     schema,
@@ -11,5 +22,5 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 app.listen(4000, () => {
-    console.log('now listening for requests on port 4000');
+    console.log('Now listening for requests on port 4000');
 });
